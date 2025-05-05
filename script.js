473febf8652227c526d7150fa9c951929b40f0cb
b1ndii <bind.berisha1@gmail.com>
2025-05-05 16:50:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+var map = L.map('map').setView([42.658869684690934, 21.161929323127325], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+
+// var marker = L.marker([42.465288840438745, 21.468990748549697]).addTo(map);
+
+// var circle = L.circle([51.508, -0.11], {
+//     color: 'green',
+//     fillColor: 'green',
+//     fillOpacity: 0.5,
+//     radius: 500
+// }).addTo(map);
+
+
+// var polygon = L.polygon([
+//     [51.509, -0.08],
+//     [51.503, -0.06],
+//     [51.51, -0.047]
+// ]).addTo(map);
+
+// marker.bindPopup('Lokacioni i jCoders: <br> <img src="https://media.licdn.com/dms/image/v2/C560BAQHeGgHBLgSWTQ/company-logo_200_200/company-logo_200_200/0/1630611987079/jcoders_logo?e=2147483647&v=beta&t=xGnTWzaoU3WNH9RuycpNOh-S8baMDcOo2w3lHRWVStU" alt=""> ').openPopup();
+// circle.bindPopup("I am a circle.");
+// polygon.bindPopup("I am a polygon.");
+
+var greenIcon = L.icon({
+    iconUrl: 'leaf-green.png',
+    shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
+
+L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+
