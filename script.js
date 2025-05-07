9a56469f3615a8f07ce424fa996f2bc3b1e316b2
b1ndii <bind.berisha1@gmail.com>
2025-05-07 16:45:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var map = L.map('map').setView([42.658869684690934, 21.161929323127325], 13);
+// var map = L.map('map').setView([42.658869684690934, 21.161929323127325], 13);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     maxZoom: 19,
+//     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+// }).addTo(map);
 
 
 // var marker = L.marker([42.465288840438745, 21.468990748549697]).addTo(map);
@@ -26,18 +26,69 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 // circle.bindPopup("I am a circle.");
 // polygon.bindPopup("I am a polygon.");
 
-var greenIcon = L.icon({
-    iconUrl: 'leaf-green.png',
-    shadowUrl: 'leaf-shadow.png',
+// var greenIcon = L.icon({
+//     iconUrl: 'leaf-green.png',
+//     shadowUrl: 'leaf-shadow.png',
 
-    iconSize:     [38, 95], // size of the icon
-    shadowSize:   [50, 64], // size of the shadow
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    shadowAnchor: [4, 62],  // the same for the shadow
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+//     iconSize:     [38, 95], // size of the icon
+//     shadowSize:   [50, 64], // size of the shadow
+//     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+//     shadowAnchor: [4, 62],  // the same for the shadow
+//     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+// });
+
+// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
+
+// L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+
+var littleton = L.marker([42.65917322650542, 21.165693430509386]).bindPopup('Prishtina'),
+    denver    = L.marker([42.463389327478964, 21.469076579236056]).bindPopup('Gilan')
+
+
+var cities = L.layerGroup([littleton, denver]);
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
 });
 
-L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
+var osmHOT = L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors, Tiles style by Humanitarian OpenStreetMap Team hosted by OpenStreetMap France'});
 
-L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map).bindPopup("I am a green leaf.");
+var map = L.map('map', {
+    center: [42.65917322650542, 21.165693430509386],
+    zoom: 10,
+    layers: [osm, cities]
+});
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "OpenStreetMap.HOT": osmHOT
+};
+
+var overlayMaps = {
+    "Cities": cities
+};
+
+var layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "<span style='color: red'>OpenStreetMap.HOT</span>": osmHOT
+};
+
+var crownHill = L.marker([42.66189745193389, 21.169184143234393]).bindPopup('Parku i Qytetit.'),
+    rubyHill = L.marker([42.67499693234073, 21.211895195910767]).bindPopup('Parku i Gërmisë.');
+    
+var parks = L.layerGroup([crownHill, rubyHill]);
+var openTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: 'Map data: © OpenStreetMap contributors, SRTM | Map style: © OpenTopoMap (CC-BY-SA)'
+});
+
+layerControl.addBaseLayer(openTopoMap, "OpenTopoMap");
+layerControl.addOverlay(parks, "Parks");
+
+
 
